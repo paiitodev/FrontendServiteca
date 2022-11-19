72af12c9ff22b1410ddab589be1ee0b0fe16f627
paiitodev <yuyis1999@hotmail.com>
2022-11-18 20:43:13 -0500

    Navbar, Footer, Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/inicio' },
+  {path: 'seguridad',loadChildren: () =>
+  import('./modulos/seguridad/seguridad.module').then(
+        (x) => x.SeguridadModule),},
+  {path: 'administradores',loadChildren: () =>
+  import('./modulos/administradores/administradores.module').then(
+        (x) => x.AdministradoresModule),},
+  {path: 'clientes',loadChildren: () =>
+  import('./modulos/clientes/clientes.module').then(
+        (x) => x.ClientesModule),},
+  {path: 'carros',loadChildren: () =>
+  import('./modulos/carros/carros.module').then(
+        (x) => x.CarrosModule),},
+  {path: 'servicios',loadChildren: () =>
+  import('./modulos/servicios/servicios.module').then(
+        (x) => x.ServiciosModule),},
+  {path: 'servicio-ventas',loadChildren: () =>
+  import('./modulos/servicio-ventas/servicio-ventas.module').then(
+        (x) => x.ServicioVentasModule),},
+  {path: '**', component: ErrorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login',component: IniciarSesionComponent
+  },
+  {
+    path: 'logout',component: CerrarSesionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
